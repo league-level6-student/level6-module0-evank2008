f38b540b47b0ee06b345e549df5ff073c5b72341
Evan Keri <Evan Keri@10.0.1.12>
2025-01-21 18:00:10 -0800

    realized how things work. finished donut shop and news api test

--- src/test/java/_03_intro_to_authenticated_APIs/NewsApiTest.java
+++ src/test/java/_03_intro_to_authenticated_APIs/NewsApiTest.java
@@ -14,6 +14,7 @@ import org.springframework.web.util.UriBuilder;
 import reactor.core.publisher.Mono;
 
 import java.net.URI;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.Function;
@@ -27,27 +28,71 @@ class NewsApiTest {
 
     NewsApi newsApi;
 
+    @Mock
+    WebClient mockClient;
+    
+    @Mock
+    Mono<ApiExampleWrapper> mockWrapper;
+    
+    @Mock
+    ResponseSpec mockRSpec;
+    @Mock
+    Article mockArticle;
+    
+    @Mock
+    RequestHeadersUriSpec mockSpec;
     @BeforeEach
     void setUp() {
-
+        MockitoAnnotations.openMocks(this);
+    	newsApi = new NewsApi();
+    	newsApi.setWebClient(mockClient);
     }
 
     @Test
     void itShouldGetNewsStoryByTopic() {
         //given
-
+    	String topic = "Teeth";
+    	ApiExampleWrapper realResults = new ApiExampleWrapper();
+    	when(mockClient.get()).thenReturn(mockSpec);
+    	when(mockSpec.uri((Function<UriBuilder,URI>)any())).thenReturn(mockSpec);
+    	when(mockSpec.retrieve()).thenReturn(mockRSpec);
+    	when(mockRSpec.bodyToMono(ApiExampleWrapper.class)).thenReturn(mockWrapper);
         //when
-
+    	when(mockWrapper.block()).thenReturn(realResults);
         //then
+    	ApiExampleWrapper expectedResults = newsApi.getNewsStoryByTopic(topic);
+    	verify(mockClient, times(1)).get();
+        assertEquals(expectedResults, realResults);
     }
 
     @Test
     void itShouldFindStory(){
         //given
+    	 String articleTitle = "How brushing your teeth regularly is RUINING your odds of getting laid";
+         String articleContent = "just kidding lmao";
+         String articleUrl = "theonion.com/articles/real";
+         Article fakeArticle = new Article();
+         fakeArticle.setTitle(articleTitle);
+         fakeArticle.setContent(articleContent);
+         fakeArticle.setUrl(articleUrl);
 
+         String topic = "Teeth";
+     	ApiExampleWrapper fakeWrapper = new ApiExampleWrapper();
+     	List<Article> aList = new ArrayList<>(1);
+     	aList.add(fakeArticle);
+     	fakeWrapper.setArticles(aList);
+     	when(mockClient.get()).thenReturn(mockSpec);
+     	when(mockSpec.uri((Function<UriBuilder,URI>)any())).thenReturn(mockSpec);
+     	when(mockSpec.retrieve()).thenReturn(mockRSpec);
+     	when(mockRSpec.bodyToMono(ApiExampleWrapper.class)).thenReturn(mockWrapper);
+     	when(mockWrapper.block()).thenReturn(fakeWrapper);
         //when
-
+     	String message =
+                articleTitle + " -\n"
+                        + articleContent
+                        + "\nFull article: " + articleUrl;
         //then
+     	assertEquals(newsApi.findStory(topic),message);
     }
 
 
--- src/test/java/_10_white_box_testing/MyDonutShopTest.java
+++ src/test/java/_10_white_box_testing/MyDonutShopTest.java
@@ -9,41 +9,78 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import _08_mocking.models.DeliveryDriver;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
+import java.util.Collections;
+import java.util.List;
+
 class MyDonutShopTest {
-
+	
     MyDonutShop myDonutShop;
-
+    Order order;
     @Mock
     DeliveryService del;
     @Mock
     PaymentService pay;
     @Mock
     BakeryService bake;
+    @Mock
+    DeliveryDriver deliveryDriver;
     @BeforeEach
-    void setUp() {
+    void setUp() { 
+    	MockitoAnnotations.openMocks(this);
 
+    	myDonutShop=new MyDonutShop(pay,del,bake);
+    	
+    	List<DeliveryDriver> availableDeliveryDrivers = Collections.singletonList(deliveryDriver);
+        myDonutShop.openForTheDay();
+        
+        del.setAvailableDeliveryDrivers(availableDeliveryDrivers);
+        order = new Order("CUSTOMER_NAME",
+    			"CUSTOMER_PHONE_NUMBER",
+                1,
+                5.00,
+                "CREDIT_CARD_NUMBER",
+                true);
+        
     }
 
     @Test
     void itShouldTakeDeliveryOrder() throws Exception {
+    	
         //given
-when()
+    	when(bake.getDonutsRemaining()).thenReturn(10);
+    	when(pay.charge(order)).thenReturn(true);
         //when
-
+    	myDonutShop.takeOrder(order);
         //then
+    	//??? doesnt work???? says its never interacted with it make no sense
+    	verify(del,times(1)).scheduleDelivery(any());
     }
 
     @Test
     void givenInsufficientDonutsRemaining_whenTakeOrder_thenThrowIllegalArgumentException() {
         //given
-
+    	when(bake.getDonutsRemaining()).thenReturn(0);
+    	when(pay.charge(order)).thenReturn(true);
         //when
-    	
+    	try {
+			myDonutShop.takeOrder(order);
+		} catch (IllegalArgumentException e) {
+			assertEquals(true,true);
+			e.printStackTrace();
+		} catch (Exception e) {
+			assertEquals(true,false);
+			e.printStackTrace();
+		}
         //then
+         verify(pay, never()).charge(any());
+         
     }
 
     @Test
@@ -51,7 +88,15 @@ when()
         //given
 
         //when
-
+    	try {
+			myDonutShop.takeOrder(order);
+		} catch(IllegalStateException e) {
+	         verify(pay, never()).charge(any());
+		}
+    	catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
         //then
     }
 
